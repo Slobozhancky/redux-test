fe6d2fcddc922529b9adf05c17219c4fe4733a9b
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-11-04 23:03:57 +0200

    feat: add logic for add and delete customer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-
-    console.log(useSelector(state => state));
+    console.log(useSelector((state) => state));
 
     let cashValue = useSelector((state) => state.cashReducer);
+    let arrOfCustomers = useSelector((state) => state.customerReducer);
+
+    console.log(arrOfCustomers);
 
     const dispatch = useDispatch();
 
-    let inp = React.useRef();
+    let inpForCash = React.useRef();
+    let inpForCustomers = React.useRef();
 
     function addCash(cash) {
         dispatch({ type: "ADD_CASH", payload: cash });
@@ -18,16 +21,43 @@ function App() {
     function getCash(cash) {
         dispatch({ type: "GET_CASH", payload: cash });
     }
+
+    function addCustomer(customer) {
+        let name = { name: customer, id: new Date() };
+        dispatch({ type: "ADD_CUSTOMER", payload: name });
+    }
+
+    function removeCustomer(customer) {
+        console.log(customer);
+        dispatch({ type: "REMOVE_CUSTOMER", payload: customer.id})
+    }
+
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <h2>Bank operations</h2>
             <p>{cashValue.cash}</p>
-            <input ref={inp} type="text" />
-            <button onClick={() => addCash(+inp.current.value)}>
+            <input ref={inpForCash} type="text" />
+            <button onClick={() => addCash(+inpForCash.current.value)}>
                 Add cash
             </button>
-            <button onClick={() => getCash(+inp.current.value)}>
+            <button onClick={() => getCash(+inpForCash.current.value)}>
                 Get cash
+            </button>
+            <br />
+            <h2>Operations with customers</h2>
+            <p>{arrOfCustomers.customer}</p>
+            <input type="text" ref={inpForCustomers} />
+            <ul>
+                {arrOfCustomers.customers.length < 1 ? (
+                    <li>Массив пустой</li>
+                ) : (
+                    arrOfCustomers.customers.map((customer, index) => (
+                        <li onClick={() => removeCustomer(customer)} key={customer.name + index}>{customer.name}</li>
+                    ))
+                )}
+            </ul>
+            <button onClick={() => addCustomer(inpForCustomers.current.value)}>
+                Add customer
             </button>
         </div>
     );
--- src/redux/customerReducer.js
+++ src/redux/customerReducer.js
@@ -4,9 +4,17 @@ let defaultState = {
 
 export let customerReducer = (state = defaultState, action) => {
     if (action.type === "ADD_CUSTOMER") {
-        return { ...state, cash: state.cash + action.payload };
-    } else if (action.type === "GET_CUSTOMER") {
-        return { ...state, cash: state.cash - action.payload };
+        return {
+            ...state,
+            customers: [...state.customers, action.payload],
+        };
+    } else if (action.type === "REMOVE_CUSTOMER") {
+        return {
+            ...state,
+            customers: state.customers.filter(
+                (customer) => customer.id !== action.payload
+            ),
+        };
     }
 
     return state;
